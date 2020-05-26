0b1edd1ec28fd1394608396d60c44634f6797600
Luna Goodnight <catsheue@gmail.com>
2020-05-26 19:31:31 +0800

    add new font :bento:

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
+import _ from 'lodash';
+import './style.css';
+import Corn from './corn.png';
+
 function component() {
 	const element = document.createElement('div');
 
-	// Lodash, currently included via a script, is required for this line to work
+	// Lodash, now imported by this script
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+	element.classList.add('hello');
+
+	// Add the image to our existing div.
+	const myCorn = new Image();
+	myCorn.src = Corn;
+	element.appendChild(myCorn);
 
 	return element;
 }
